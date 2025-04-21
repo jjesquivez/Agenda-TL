359af365d5df1f789ca5bfb9ffd167a35633138c
jjesquivez <75186091+jjesquivez@users.noreply.github.com>
2025-04-20 21:10:00 -0600

    Delete Agenda directory

--- Agenda/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AgendaJesusTalentLand from './AgendaJesusTalentLand'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AgendaJesusTalentLand />
-  </React.StrictMode>,
-)
